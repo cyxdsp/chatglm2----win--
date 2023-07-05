408cacbae60d56ca0e18e7ab464aa5b83367799f
cyxdsp <103276798+cyxdsp@users.noreply.github.com>
2023-07-05 17:17:10 +0800

    Add files via upload

---
+++ train.sh
@@ -0,0 +1,26 @@
+PRE_SEQ_LEN=128
+LR=1e-3
+NUM_GPUS=0
+
+#torchrun --standalone --nnodes=1 --nproc-per-node=$NUM_GPUS main.py \
+python main.py \
+    --do_train \
+    --train_file AdvertiseGen/train.json \
+    --validation_file AdvertiseGen/dev.json \
+    --preprocessing_num_workers 10 \
+    --prompt_column content \
+    --response_column summary \
+    --model_name_or_path THUDM/chatglm2-6b \
+    --output_dir output/adgen-chatglm2-6b-pt-$PRE_SEQ_LEN-$LR \
+    --overwrite_output_dir \
+    --max_source_length 128 \
+    --max_target_length 128 \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 1 \
+    --gradient_accumulation_steps 16 \
+    --predict_with_generate \
+    --max_steps 90 \
+    --logging_steps 10 \
+    --save_steps 90 \
+    --learning_rate $LR \
+    --pre_seq_len $PRE_SEQ_LEN \
